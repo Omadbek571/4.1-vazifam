a4ac510bcde1b389578e38bdffbc10d6301472ac
Omadbek571 <omadbektomsk08@gmail.com>
2024-05-16 19:12:08 +0500

    .

---
+++ app.js
@@ -0,0 +1,150 @@
+// 1-savol
+
+// JAVOBI
+
+// let L = +prompt("ILTIMOS LITIR KIRITING")
+// let natija = L * 100;
+// document.write("Santimetiri metirdi  " + Math.floor(natija));
+
+
+// 2-savol
+
+// JAVOBI
+
+// let M = +prompt("ILTIMOS TONNA KIRITING")
+// let natija = M * 1000;
+// document.write("Tonna kilogrami  " + Math.floor(natija));
+
+
+// 3-savol
+
+// JAVOBI
+
+// let Kb = +prompt("ILTIMOS KB KIRDIRING");
+// let natija = Kb * 1024
+// document.write("KB NING BAYT GA OZGARTIRILGANDA" + (natija))
+
+
+// 4-savol
+
+// JAVOB
+
+// let A = +prompt("ILTIMOS A NUQTANI YOZING");
+// let B = +prompt("ILTIMOS B NUQTANI YOZING");
+
+// let natija = Math.floor(A/B)
+// document.write("A kesmada B kesmani " + natija + " marta joylashtirish mumkin.")    
+
+// 5-savol
+
+// JAVOB
+
+
+// let A = 5;
+// let B = 4;
+
+// let natija = A % B
+
+// document.write("A kesmada B kesmani " + Math.floor(A / B) + " marta joylashtirish mumkin.");
+// document.write("A kesmada B kesmaning joylashmagan qismi: " + natija);
+
+
+// 6-savol
+
+// JAVOBI
+// let son = +prompt("Ikki xonali son kiriting:");
+// let onliklar = Math.floor(son / 10); 
+// let birlar = son % 10; 
+
+// document.write("O'nliklar xonasidagi raqam: " + onliklar);
+// document.write("Birlar xonasidagi raqam: " + birlar);
+
+
+// 7-savol
+
+// JAVOBI
+
+// let son = +prompt("Ikki xonali son kiriting:");
+// let birliklar = son % 10;
+// let onliklar = Math.floor(son / 10); 
+// let natija = birliklar + onliklar; 
+
+// document.write("Ikki xonali son raqamlarining yig'indisi: " + natija);
+
+
+// 8-savol
+
+// JAVOBI
+
+// let son = +prompt("ILTIMOS 2 XONALI SON KIRITING");
+// let birliklar = son % 10;
+// let onliklar = Math.floor(son / 10);
+
+// let natija = onliklar + birliklar
+
+// document.write(natija);
+
+
+// 9-savol
+
+// JAVOBI
+
+
+// let son = +prompt("Uch xonali son kiriting:");
+// let yuzlar = Math.floor(son / 100) % 10;
+
+// document.write("Uch xonali sonning yuzlar xonasidagi raqami: " + yuzlar);
+
+
+// 10-savol
+
+// NATIJA
+
+// let son = +prompt("Uch xonali son kiriting:");
+// let birliklar = son % 10; 
+// let onliklar = Math.floor((son % 100) / 10); 
+
+// console.log("Birliklar xonasidagi raqam: " + birliklar);
+// console.log("O'nliklar xonasidagi raqam: " + onliklar);
+
+// 11-savol
+
+// JAVOBI
+
+// let son  = 171;
+// let birliklar = son % 10; 
+// let onliklar = Math.floor((son % 100) / 10); 
+// let yuzlar = Math.floor(son / 100);
+
+// let natija = birliklar + onliklar + yuzlar;
+
+// document.write(natija);
+
+
+// 12-savol
+
+// JAVOBI
+
+// let son = +prompt("Uch xonali son kiriting:");
+// let birliklar = son % 10;
+// let onliklar = Math.floor((son % 100) / 10); 
+// let yuzlar = Math.floor(son / 100); 
+
+// let teskariSon = birliklar * 100 + onliklar * 10 + yuzlar; 
+
+// document.write("Uch xonali sonning teskari tartibdagi soni: " + teskariSon); 
+
+
+// 13-savol
+
+// NATIJA
+
+// let son = +prompt("Uch xonali son kiriting:");
+// let birliklar = son % 10; 
+// let onliklar = Math.floor((son % 100) / 10); 
+// let yuzlar = Math.floor(son / 100);
+
+// let yangiSon = birliklar * 100 + onliklar * 10 + yuzlar;
+
+// document.write("Uch xonali sonning chapdan birinchi raqamini o'chirib o'ng tarafiga yozilgan son: " + yangiSon);
+
